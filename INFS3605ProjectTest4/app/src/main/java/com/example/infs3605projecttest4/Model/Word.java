d31d27b27c90d4cdcc666e4da44d6ef91e52634c
AndechengLiu <a2532838768@gmail.com>
2020-04-07 10:58:08 +1000

    alph

--- INFS3605ProjectTest4/app/src/main/java/com/example/infs3605projecttest4/Model/Word.java
+++ INFS3605ProjectTest4/app/src/main/java/com/example/infs3605projecttest4/Model/Word.java
@@ -1,9 +1,18 @@
 package com.example.infs3605projecttest4.Model;
 
+import com.example.infs3605projecttest4.R;
+
+import java.util.ArrayList;
+
 public class Word {
     private String english;
     private int image;
     private String local;
+    private String rule;
+    private String noongarExample;
+    private String englishExample;
+
+    private static ArrayList<Word> allWordList = new ArrayList<>();
 
     public Word(String english, int image, String local) {
         this.english = english;
@@ -34,4 +43,19 @@ public class Word {
     public void setLocal(String local) {
         this.local = local;
     }
+
+    public static ArrayList<Word> getAllWordList() {
+        return allWordList;
+    }
+
+    public static void setAllWordList(ArrayList<Word> allWordList) {
+        Word.allWordList = allWordList;
+    }
+
+    static {
+        allWordList.add(new Word("Boy", R.drawable.boy,"Nop"));
+        allWordList.add(new Word("Boy", R.drawable.boy,"Nop"));
+    }
+
+
 }

6ca25d5b4646858827d9b7a7cd195b2b5592e697
AinterestingAllan <55994729+AinterestingAllan@users.noreply.github.com>
2020-04-11 17:47:01 +1000

    missing word funtion

--- INFS3605ProjectTest4/app/src/main/java/com/example/infs3605projecttest4/Model/TestType.java
+++ INFS3605ProjectTest4/app/src/main/java/com/example/infs3605projecttest4/Model/TestType.java
@@ -8,6 +8,7 @@ public class TestType {
     private String name;
     private ArrayList<Word> wordList = new ArrayList<>();
     private ArrayList<Sentence> sentences = new ArrayList<>();
+    private ArrayList<WordGroup> wordGroupList = new ArrayList<>();
 
     public TestType(String name) {
         this.name = name;
@@ -32,14 +33,19 @@ public class TestType {
     private static ArrayList<TestType> typeList = new ArrayList<>();
 
     static {
+        // types
         typeList.add(new TestType("Family"));
         typeList.add(new TestType("XXXX2"));
         typeList.add(new TestType("XXXX3"));
         typeList.add(new TestType("XXXX4"));
+
+        // type's wordlist
         typeList.get(0).wordList.add(new Word("Boy", R.drawable.boy,"Nop"));
         typeList.get(0).wordList.add(new Word("Girl", R.drawable.girl,"Koort"));
         typeList.get(0).wordList.add(new Word("Man", R.drawable.man,"Noongar"));
         typeList.get(0).wordList.add(new Word("Woman", R.drawable.woman,"Yoka"));
+
+        // type's sentencelist
         ArrayList<Word> s1 = new ArrayList<>();
         ArrayList<Word> s2 = new ArrayList<>();
         ArrayList<Word> s3 = new ArrayList<>();
@@ -60,6 +66,20 @@ public class TestType {
         typeList.get(0).sentences.add(new Sentence(1, s2,"C B A"));
         typeList.get(0).sentences.add(new Sentence(1, s3,"A C B"));
         typeList.get(0).sentences.add(new Sentence(1, s4,"B A C"));
+
+
+        // type's wordgrouplist
+        WordGroup wordGroup0 = new WordGroup(new Word("xxxx", R.drawable.boy,"WordA")
+                ,new Word("xxxx", R.drawable.boy,"WordB"));
+        WordGroup wordGroup1 = new WordGroup(new Word("xxxx", R.drawable.boy,"HahaA")
+                ,new Word("xxxx", R.drawable.boy,"HahaB"));
+        WordGroup wordGroup2 = new WordGroup(new Word("xxxx", R.drawable.boy,"ExampleA")
+                ,new Word("xxxx", R.drawable.boy,"ExampleB"));
+        typeList.get(0).wordGroupList.add(wordGroup0);
+        typeList.get(0).wordGroupList.add(wordGroup1);
+        typeList.get(0).wordGroupList.add(wordGroup2);
+
+
     }
 
     public static ArrayList<TestType> getTypeList() {
@@ -73,4 +93,18 @@ public class TestType {
     public void setSentences(ArrayList<Sentence> sentences) {
         this.sentences = sentences;
     }
+
+    public ArrayList<WordGroup> getWordGroupList() {
+        return wordGroupList;
+    }
+
+    public void setWordGroupList(ArrayList<WordGroup> wordGroupList) {
+        this.wordGroupList = wordGroupList;
+    }
+
+    public static void setTypeList(ArrayList<TestType> typeList) {
+        TestType.typeList = typeList;
+    }
+
+
 }
--- INFS3605ProjectTest4/app/src/main/java/com/example/infs3605projecttest4/SelectActivity.java
+++ INFS3605ProjectTest4/app/src/main/java/com/example/infs3605projecttest4/SelectActivity.java
@@ -14,6 +14,7 @@ public class SelectActivity extends AppCompatActivity {
     Button pictionaryBt;
     Button textBt;
     Button sentenceBt;
+    Button missingBt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +24,7 @@ public class SelectActivity extends AppCompatActivity {
         pictionaryBt = findViewById(R.id.select_pctionary);
         textBt = findViewById(R.id.select_text);
         sentenceBt = findViewById(R.id.select_sentence);
+        missingBt = findViewById(R.id.select_missing);
 
         pictionaryBt.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,6 +36,7 @@ public class SelectActivity extends AppCompatActivity {
         textBt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                TextActivity.TAG = "text";
                 goActivity(TextActivity.class);
             }
         });
@@ -42,6 +45,14 @@ public class SelectActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 goActivity(SentenceActivity.class);
+            }
+        });
+
+        missingBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextActivity.TAG = "group";
+                goActivity(TextActivity.class);
             }
         });
 
